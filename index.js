442c8293ccf89f948e5fccf7dfc7e9b3c41a9853
Laura Fretter <fretterlaura@gmail.com>
2019-02-26 07:29:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Laura"
-var height = "55"
+var height = 55
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 
-console.log(parseInt(height)+ 1)
+console.log(height+ 1)
